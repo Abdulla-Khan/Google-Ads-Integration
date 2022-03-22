8886b70519192ff4ffcb1d00633568493b9c0482
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-03-23 01:38:52 +0500

    Commit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  MobileAds.instance.initialize();
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
-}
-
 const int maxAttempts = 3;
 
 class MyApp extends StatefulWidget {
